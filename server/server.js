d35b760f0cf67993770c7a6028fe6d871faaa1cf
Juanma <jmlpdr.93@gmail.com>
2020-11-07 20:41:35 +0000

    :memo: update documentation in server.js

--- server/server.js
+++ server/server.js
@@ -3,9 +3,12 @@ import app from './express';
 import mongoose from 'mongoose';
 
 /*
-Definir esquemas con datos fuertemente tipados. Una vez que se define un esquema, Mongoose le permite 
-crear un Modelo basado en un esquema específico. Un modelo de mongoose se asigna a un documento MongoDB 
-a través de la definición del esquema del modelo.
+Mongoose es una herramienta de modelado de objetos de MongoDB que proporciona una solución basada en 
+esquemas para modelar datos de aplicaciones. Incluye conversión de tipos, validación, creación de consultas 
+y enlaces de lógica empresarial integrados. El uso de Mongoose con esta pila de backend proporciona una capa 
+superior a MongoDB con más funcionalidad, incluida la asignación de modelos de objetos a documentos 
+de base de datos. Esto hace que sea más sencillo y productivo desarrollar con un servidor Node y MongoDB. 
+Para obtener más información sobre Mongoose, visite mongoosejs.com.
 */
 
 mongoose.Promise = global.Promise;
