cf7a5556e72818abc187d8e301289039378371af
Juanma <jmlpdr.93@gmail.com>
2020-11-07 23:17:29 +0000

    :wrench::truck: add file auth.routes.js

--- server/express.js
+++ server/express.js
@@ -5,7 +5,8 @@ import compress from 'compression';
 import cors from 'cors';
 import helmet from 'helmet';
 import Template from './../template';
-import userRoutes from './routes/user.routes'
+import userRoutes from './routes/user.routes';
+import authRoutes from './routes/auth.routes';
 
 /*
 - body-parser: Solicite el middleware de análisis del cuerpo para manejar las complejidades del análisis 
@@ -38,6 +39,7 @@ app.use(compress());
 app.use(helmet());
 app.use(cors());
 app.use('/', userRoutes);
+app.use('/', authRoutes);
 
 /*
 Endpoint de controlador
